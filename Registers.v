379c06bf8bf5f66023f3882f2b3cb06f22f45959
gaozuqiang <68024818+gaozuqiang@users.noreply.github.com>
2025-08-02 19:19:13 -0400

    Update Registers.v

--- Registers.v
+++ Registers.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/14/2025 10:12:13 PM
-// Design Name: 
-// Module Name: Registers
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Registers(
     input [4:0] readregister1,
