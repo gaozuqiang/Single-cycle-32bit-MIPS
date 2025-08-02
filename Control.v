2a5ba0c26f636eae5705f0f87039503d002bf031
gaozuqiang <68024818+gaozuqiang@users.noreply.github.com>
2025-08-02 19:19:52 -0400

    Update Control.v

--- Control.v
+++ Control.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/16/2025 07:33:25 PM
-// Design Name: 
-// Module Name: Control
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Control(
     input [5:0]Op,
