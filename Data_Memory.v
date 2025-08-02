928b5c37f2702c633cbaf477d218c4b6abc77ac0
gaozuqiang <68024818+gaozuqiang@users.noreply.github.com>
2025-08-02 19:19:42 -0400

    Update Data_Memory.v

--- Data_Memory.v
+++ Data_Memory.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/14/2025 10:37:27 PM
-// Design Name: 
-// Module Name: Data_Memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Data_Memory(
     input [5:0] Address,
