58405a107b491b18972de9260997ecd27581adab
gaozuqiang <68024818+gaozuqiang@users.noreply.github.com>
2025-08-02 19:19:26 -0400

    Update Instruction_Memory.v

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/14/2025 10:02:36 PM
-// Design Name: 
-// Module Name: Instruction_Memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Instruction_Memory(
     input [5:0]Read_address, // Address of the Instruction max 64 instructions
