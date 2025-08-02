7245032b0943c49905df323451dfc4d9dc0d8a4a
gaozuqiang <68024818+gaozuqiang@users.noreply.github.com>
2025-08-02 19:20:02 -0400

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/14/2025 10:36:54 PM
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ALU(
     input  [31:0] a,
